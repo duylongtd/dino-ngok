8bdc770d0f0b8643f68c0a23f1b5bebc0f5a0c25
unknown <tranlong280403@gmail.com>
2023-12-17 14:35:13 +0700

    fix: Cors

--- src/constant/Const.tsx
+++ src/constant/Const.tsx
@@ -1,5 +1,5 @@
 enum ICons {
-    "FETCH_PRODUCTS" = "src/api/Product.json" 
+    "FETCH_PRODUCTS" = "https://firebasestorage.googleapis.com/v0/b/long-823d0.appspot.com/o/Product.json?alt=media&token=649a7e4a-4de5-44f8-bcdc-5236699e9d2a" 
 }
 
 export default ICons;
