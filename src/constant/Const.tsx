eea9de6485f2d754fa3d62c4513cbc0d8bb2300d
unknown <tranlong280403@gmail.com>
2023-12-17 15:14:52 +0700

    Stop tracking src/constant/Const.tsx

--- src/constant/Const.tsx
+++
@@ -1,5 +0,0 @@
-enum ICons {
-    "FETCH_PRODUCTS" = "https://firebasestorage.googleapis.com/v0/b/long-823d0.appspot.com/o/Product.json?alt=media&token=649a7e4a-4de5-44f8-bcdc-5236699e9d2a" 
-}
-
-export default ICons;
